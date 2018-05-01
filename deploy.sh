5ca6a60c63d33a5dcff3f78a25edf80518e19f79
LuisSas <luis@makingdevs.com>
2018-04-30 20:48:29 -0500

    Agregando submodulo de public

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+echo "1.- Add some text to make a great commit"
+read comm
+
+echo "2.- Adding a pushing your changes"
+git add -A
+git commit -m "$comm"
+git push origin master
+
+echo "3.- Repository updated"
+
+echo "3.- Updating a static blog content"
+rm -rf public/*
+hugo --buildDrafts
+cd public
+git checkout master
+touch CNAME
+echo luissas.me >> CNAME
+git checkout master
+git add -A
+git commit -m "$comm"
+git push origin master --force
+echo "4.- Changes in your blog D:"
+cd ..
+git add -A
+git commit -m "$comm"
+git push origin master --force
+echo ":D"
